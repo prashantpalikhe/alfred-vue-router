8c723462a7276340741f4c78827437e08cf63f9d
Prashant Palikhe <prashpal09@gmail.com>
2019-09-02 15:43:31 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const alfy = require('alfy');
+const algoliasearch = require('algoliasearch');
+const { last } = require('lodash');
+
+const client = algoliasearch(
+	'BH4D9OD16A',
+	'f854bb46d3de7eeb921a3b9173bd0d4c'
+);
+const index = client.initIndex('vue-router');
+
+(async () => {
+	const { hits } = await index.search({
+		query: alfy.input,
+		hitsPerPage: 5
+	});
+
+	const output = hits.map(hit => {
+		const result = {
+			title: hit.anchor,
+			subtitle: hit.anchor,
+			arg: hit.url,
+			quicklookurl: hit.url
+		};
+
+		if (hit.hierarchy) {
+			const hierarchies = Object.values(hit.hierarchy).filter(Boolean);
+
+			result.title = last(hierarchies);
+			result.subtitle = hierarchies.join(' > ');
+		}
+
+		return result;
+	});
+
+	alfy.output(output);
+})();
---
+++ readme.md
@@ -0,0 +1,22 @@
+# alfred-vue-router [![Build Status](https://travis-ci.org/prashantpalikhe/alfred-vue-router.svg?branch=master)](https://travis-ci.org/prashantpalikhe/alfred-vue-router)
+
+> Alfred workflow for Vue router
+
+
+## Install
+
+```
+$ npm install --global alfred-vue-router
+```
+
+*Requires [Node.js](https://nodejs.org) 4+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
+
+
+## Usage
+
+In Alfred, type `vuerouter`, <kbd>Enter</kbd>, and your query.
+
+
+## License
+
+MIT © [Prashant Palikhe](https://twitter.com/prashantpalikhe)
